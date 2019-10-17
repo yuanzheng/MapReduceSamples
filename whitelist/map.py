c9d32603fc9a816f50a0a85f3a92bd4ec4d8a64c
Lucas Song <yuanzheng@cs.byu.edu>
2019-10-18 05:14:19 +0800

    mapper is done

--- whitelist/map.py
+++ whitelist/map.py
@@ -2,8 +2,28 @@
 
 import sys
 
-def test(wordList):
-    print (wordList)
+def read_file(f):
+    word_set = set()
+    file_in = open(f, 'r')
+
+    for line in file_in:
+        word_set.add(line.strip())
+
+    return word_set
+
+
+
+def mapper_func(wordList):
+    word_set = read_file(wordList)
+
+    for line in sys.stdin:
+        ss = line.strip().split(' ')
+        
+        for each in ss:
+            word = each.strip()
+            if word != "" and (word in word_set):
+                print('\t'.join([word, '1']))
+
 
 
 if __name__=="__main__":
@@ -16,5 +36,5 @@ if __name__=="__main__":
 
     if len(sys.argv) > 1:
         args = sys.argv[2:]
-
+    print(args)
     func(*args)
