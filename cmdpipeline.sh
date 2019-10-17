f807c688a9cd60f160b27a5fbd8c04794b25e524
Lucas Song <yuanzheng@cs.byu.edu>
2019-10-18 04:47:25 +0800

    re-orgnized wordcound sample by moving into wordcount file

--- cmdpipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo "hello world"
-
-
-#head -2 ../data/The_Man_of_Property.txt | python map_new.py | sort | python red_new.py | wc -l
-cat ../data/The_Man_of_Property.txt | python map_new.py | sort | python red_new.py | wc -l
-
